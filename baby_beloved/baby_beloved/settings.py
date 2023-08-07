3399348019059b5aa14285e01c4f5c88c646a4d7
TheDKnight <>
2023-08-07 16:59:33 +0000

    updated email settings

--- baby_beloved/baby_beloved/settings.py
+++ baby_beloved/baby_beloved/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = True 
 
 ALLOWED_HOSTS = ['localhost',
-                 '8000-thedknight-pp5-ecom-baby-nm97ays0jb.us2.codeanyapp.com', 'pp5-ecom-final-bb6504664877.herokuapp.com', '8000-thedknight-pp5-ecom-baby-tzl2dbov7s.us2.codeanyapp.com/']
+                 '8000-thedknight-pp5-ecom-baby-nm97ays0jb.us2.codeanyapp.com', 'pp5-ecom-final-bb6504664877.herokuapp.com']
 
 
 # Application definition
@@ -222,4 +222,16 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = env('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = env('STRIPE_SECRET_KEY')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET')
-DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = env('EMAIL_HOST_USER')
