f0ef82005aa515eed78282280a53bea374c1e205
TheDKnight <>
2023-09-07 22:18:06 +0000

    added product reviews

--- baby_beloved/products/models.py
+++ baby_beloved/products/models.py
@@ -1,5 +1,5 @@
+from django.contrib.auth.models import User
 from django.db import models
-
 
 class Category(models.Model):
 
@@ -31,3 +31,11 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, related_name = 'reviews', on_delete=models.CASCADE)
+    rating = models.IntegerField(default=3)
+    content = models.TextField()
+    created_by = models.ForeignKey(User, related_name = 'reviews', on_delete=models.CASCADE )
+    created_on = models.DateTimeField(auto_now_add = True)
